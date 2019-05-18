02bda2b7c7b8bd3d99a264cbb644d130a59edda5
Janaina <janaina.militao@gmail.com>
2019-05-18 11:33:52 -0300

    Conexão ftp

--- src/main/java/com/marketpay/hackathon/SFTPConexao.java
+++ src/main/java/com/marketpay/hackathon/SFTPConexao.java
@@ -4,23 +4,18 @@ import com.jcraft.jsch.JSchException;
 import com.marketpay.hackathon.model.ConfigSFTP;
 
 public class SFTPConexao {
-	
-	
-	private static final String USERNAME = "mptechday";
-    private static final String HOST = "10.50.4.5";
-    private static final int PORT = 22021;
-    private static final String PASSWORD = "mptechday@2019*";
 
-	public static void main(String[] args) throws IllegalAccessException, JSchException {
-		
+
+	private static final String USERNAME = "mptechday";
+	private static final String HOST = "10.50.4.5";
+	private static final int PORT = 22021;
+	private static final String PASSWORD = "mptechday@2019*";
+
+	public void consultarArquivos() throws IllegalAccessException, JSchException {
+
 		ConfigSFTP sshConnector = new ConfigSFTP();
-             
-             
-            sshConnector.connect(USERNAME, PASSWORD, HOST, PORT);
-            System.out.println("CONECTOU");
-            sshConnector.disconnect();
-             
-			
+		sshConnector.connect(USERNAME, PASSWORD, HOST, PORT);
+		sshConnector.disconnect();
 	}
 
 }
--- src/main/java/com/marketpay/hackathon/model/ConfigSFTP.java
+++ src/main/java/com/marketpay/hackathon/model/ConfigSFTP.java
@@ -10,29 +10,29 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ConfigSFTP {
-	
-	 private Session session;
-	 
-	 public void connect(String username, String password, String host, int port)
-		        throws JSchException, IllegalAccessException {
-		        if (this.session == null || !this.session.isConnected()) {
-		            JSch jsch = new JSch();
-		 
-		            this.session = jsch.getSession(username, host, port);
-		            this.session.setPassword(password);
-		 
-		            // Parametro para no validar key de conexão.
-		            this.session.setConfig("StrictHostKeyChecking", "no");
-		 
-		            this.session.connect();
-		            System.out.println("Comunicacao estabelecida");
-		        } else {
-		            throw new IllegalAccessException("Comunicaçao não estabelecida");
-		        }
-		    }
-	 
-	 public final void disconnect() {
-	        this.session.disconnect();
-	    }
-		 
+
+	private Session session;
+
+	public void connect(String username, String password, String host, int port)
+			throws JSchException, IllegalAccessException {
+		if (this.session == null || !this.session.isConnected()) {
+			JSch jsch = new JSch();
+
+			this.session = jsch.getSession(username, host, port);
+			this.session.setPassword(password);
+
+			// Parametro para no validar key de conexão.
+			this.session.setConfig("StrictHostKeyChecking", "no");
+
+			this.session.connect();
+			System.out.println("Comunicacao estabelecida");
+		} else {
+			throw new IllegalAccessException("Comunicaçao não estabelecida");
+		}
+	}
+
+	public final void disconnect() {
+		this.session.disconnect();
+	}
+
 }
